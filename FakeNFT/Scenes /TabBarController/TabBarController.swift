682d25c70bc11dd99bf8d96bcc7e2720e837bbac
alev-ti <kainah999@yandex.ru>
2025-04-21 21:50:34 +0200

    feat: add cart to tab bar

--- FakeNFT/Scenes /TabBarController/TabBarController.swift
+++ FakeNFT/Scenes /TabBarController/TabBarController.swift
@@ -9,17 +9,42 @@ final class TabBarController: UITabBarController {
         image: UIImage(systemName: "square.stack.3d.up.fill"),
         tag: 0
     )
+    
+    private let cartTabBarItem = UITabBarItem(
+        title: NSLocalizedString("Tab.cart", comment: ""),
+        image: UIImage(named: "tab_bar_cart_inactive"),
+        selectedImage: UIImage(named: "tab_bar_cart_active")
+    )
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let catalogController = TestCatalogViewController(
-            servicesAssembly: servicesAssembly
-        )
-        catalogController.tabBarItem = catalogTabBarItem
-
-        viewControllers = [catalogController]
-
+        configureAppearance()
+        setupViewControllers()
+    }
+    
+    private func configureAppearance() {
         view.backgroundColor = .systemBackground
+    }
+    
+    private func setupViewControllers() {
+        let catalogViewController = makeCatalogViewController()
+        let cartViewController = makeCartViewController()
+        
+        viewControllers = [catalogViewController, cartViewController]
+    }
+    
+    private func makeCatalogViewController() -> UIViewController {
+        let controller = TestCatalogViewController(servicesAssembly: servicesAssembly)
+        controller.tabBarItem = catalogTabBarItem
+        return controller
+    }
+    
+    private func makeCartViewController() -> UIViewController {
+        let assembly = CartAssembly(servicesAssembly: servicesAssembly)
+        let controller = assembly.assemble()
+        let navigationController = UINavigationController(rootViewController: controller)
+        navigationController.modalPresentationStyle = .fullScreen
+        navigationController.tabBarItem = cartTabBarItem
+        return navigationController
     }
 }
