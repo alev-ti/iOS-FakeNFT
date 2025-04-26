6f339f2eefd184c49f7b05087635f5e6bbdbae8d
alev-ti <kainah999@yandex.ru>
2025-04-26 21:18:55 +0200

    feat: add success purchase vc, presenter

---
+++ FakeNFT/Scenes/Cart/SuccessPurchase/SuccessPurchaseAssembly.swift
@@ -0,0 +1,11 @@
+import Foundation
+import UIKit
+
+final class SuccessPurchaseAssembly {
+    func assemble() -> UIViewController {
+        let presenter = SuccessPurchasePresenter()
+        let viewController = SuccessPurchaseViewController(presenter: presenter)
+        presenter.view = viewController
+        return viewController
+    }
+}
