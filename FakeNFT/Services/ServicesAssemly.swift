9bfca0541d118df71a27e6c86bccd73490fe2c14
alev-ti <kainah999@yandex.ru>
2025-04-26 21:21:54 +0200

    feat: implement payment, select currency screens

--- FakeNFT/Services/ServicesAssemly.swift
+++ FakeNFT/Services/ServicesAssemly.swift
@@ -2,19 +2,29 @@ final class ServicesAssembly {
 
     private let networkClient: NetworkClient
     private let nftStorage: NftStorage
+    private let currencyStorage: CurrencyStorageProtocol
 
     init(
         networkClient: NetworkClient,
-        nftStorage: NftStorage
+        nftStorage: NftStorage,
+        currencyStorage: CurrencyStorageProtocol
     ) {
         self.networkClient = networkClient
         self.nftStorage = nftStorage
+        self.currencyStorage = currencyStorage
     }
 
     var nftService: NftService {
         NftServiceImpl(
             networkClient: networkClient,
             storage: nftStorage
+        )
+    }
+    
+    var currencyService: CurrencyServiceProtocol {
+        CurrencyService(
+            networkClient: networkClient,
+            storage: currencyStorage
         )
     }
 }
